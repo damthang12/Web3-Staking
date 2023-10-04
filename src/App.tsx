bbc89804409202222d6e59d83e41ec569a34b11b
thaito <thai.to@kardiachain.io>
2023-10-04 15:36:50 +0700

    Rework contract interaction

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import React from "react";
 import "./App.css";
 import { Web3ReactProvider } from "@web3-react/core";
-import { ethers } from "ethers";
 import StakingController from "./StackingController";
+import { Web3Provider } from "@ethersproject/providers";
+import { ethers } from "ethers";
 
 function getLibrary(provider: any) {
     return new ethers.providers.Web3Provider(provider);
 }
-
+// export const getLibrary = (provider: any): Web3Provider => {
+//   const library = new Web3Provider(provider)
+//   library.pollingInterval = 12000
+//   return library
+// }
 function App() {
     return (
         <Web3ReactProvider getLibrary={getLibrary}>
--- src/StackingController.tsx
+++ src/StackingController.tsx
@@ -1,70 +1,19 @@
 import React, { useState } from "react";
-import { ethers } from "ethers";
-import Web3 from "web3";
-import stakingContractAbi from './contractAbi'
+import { useAuth } from "./hooks/useAuth";
+import { useWeb3React } from "@web3-react/core";
+import { useStaking } from "./hooks/useStaking";
 
-const web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
 
 function StakingController() {
-    const [defaultAccount, setDefaultAccount] = useState(null);
-    const [userBalance, setUserBalance] = useState<string>();
-    const [number, setNumber] = useState("");
-    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const { account } = useWeb3React()
+    const { login } = useAuth()
+    const { onStake } = useStaking()
+    const [userBalance, setUserBalance] = useState('')
 
-    const handleChange = (event: any) => {
-        setNumber(event.target.value);
-    };
-    const connectWalletHandler = () => {
-        if (window.ethereum) {
-            provider.send("eth_requestAccounts", []).then(async () => {
-                await accountChangedHandler(provider.getSigner());
-            });
-        } else {
-            // setErrorMessage("Please Install Metamask!!!");
-            console.log("Please Install Metamask");
-        }
-    };
+    const handleStaking = () => {
 
-    const accountChangedHandler = async (newAccount: any) => {
-        const address = await newAccount.getAddress();
-        setDefaultAccount(address);
-        const balance = await newAccount.getBalance();
-        const convertBalance = web3.utils.fromWei(balance, "ether");
-
-        setUserBalance(convertBalance);
-        await getUserBalance(address);
-        console.log(getFromContract);
-    };
-    const getUserBalance = async (address: any) => {
-        const balance = await provider.getBalance(address, "latest");
-    };
-
-    // Get the contract address
-    const contractAddress = "0xaB7a043B7BEEc9A00EB649CdC93310aDB2c22E52";
-
-    // Create an instance of the contract
-    const contract = new web3.eth.Contract(stakingContractAbi, contractAddress);
-    console.log(contract);
-
-    async function getCurrentAccount() {
-        const accounts = await web3.eth.getAccounts();
-        return accounts[0];
     }
 
-    async function getInputNumber() {
-        const amountWei = web3.utils.toWei(number, "ether");
-        return amountWei;
-    }
-
-    async function getFromContract() {
-        const account = await getCurrentAccount();
-        const amount = await getInputNumber();
-
-        const stack = await contract.methods["stake"](
-            "0x90386996f314342Fb8CdC2bd672F751Da4184a1e"
-        ).send({ from: amount });
-        console.log(stack);
-    }
 
     return (
         <div className="App">
@@ -87,15 +36,15 @@ function StakingController() {
                                     <div className="text-base">
                                         Staking amount
                                     </div>
-                                    <input
-                                        className="input-container"
-                                        placeholder="Ex: 1000"
-                                        type="number"
-                                        id="number"
-                                        name="number"
-                                        onChange={handleChange}
-                                        value={number}
-                                    ></input>
+                                    {/*<input*/}
+                                    {/*    className="input-container"*/}
+                                    {/*    placeholder="Ex: 1000"*/}
+                                    {/*    type="number"*/}
+                                    {/*    id="number"*/}
+                                    {/*    name="number"*/}
+                                    {/*    onChange={handleChange}*/}
+                                    {/*    value={number}*/}
+                                    {/*></input>*/}
                                 </div>
                             </div>
                             <div className="percen-container">
@@ -114,15 +63,17 @@ function StakingController() {
                             <div className="button-connect-waraper">
                                 <button
                                     className="button-connect"
-                                    onClick={connectWalletHandler}
+                                    onClick={login}
                                 >
-                                    {defaultAccount ? "Connected!!" : "Connect"}
+                                    {account ? "Connected!!" : "Connect"}
                                 </button>
 
                                 <button
                                     className="button-connect"
-                                    onClick={getFromContract}
-                                ></button>
+                                    onClick={handleStaking}
+                                >
+                                    Stake
+                                </button>
                             </div>
                         </div>
                     </div>
